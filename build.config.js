c88b3f968b495e900f46682cd8279428cc8eb752
Mazel <mazuel.loic@gmail.com>
2018-07-31 16:06:11 +0200

    :fire: Moving Marker : First commit

---
+++ build.config.js
@@ -0,0 +1,11 @@
+var nodeExternals = require('webpack-node-externals');
+
+module.exports = {
+  webpack: {
+    externals: [
+      nodeExternals(),
+      {'leaflet': 'L'},
+      {'leaflet.movingmarker': {root: ['leaflet', 'movingmarker']}}
+    ]
+  }
+}
